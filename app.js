0805290dd7e6251d58d06fe50207de842175c9ad
unknown <sam7073@naver.com>
2020-09-09 07:04:24 +0900

    9/9 nodeBird front clone completed

--- app.js
+++ app.js
@@ -48,5 +48,5 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 app.listen(app.get("port"), () => {
-  console.nextlog(app.get("port"), "번 포트에서 대기 중");
+  console.log(app.get("port"), "번 포트에서 대기 중");
 });
